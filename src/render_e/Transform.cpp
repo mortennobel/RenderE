cf88aeea1a66275b270671952edc5840440328ea
morten@nobel-joergensen.com <morten@nobel-joergensen.com@75417771-6e0e-4020-8c6c-34838eb40c82>
2010-11-14 11:49:45 +0000

    Fix transform (transform and inverse transform was not implemented)
    git-svn-id: https://free1.projectlocker.com/mortennobel/RenderE/svn/trunk@14 75417771-6e0e-4020-8c6c-34838eb40c82

--- src/render_e/Transform.cpp
+++ src/render_e/Transform.cpp
@@ -12,7 +12,7 @@
 namespace render_e {
 
 Transform::Transform(void)
-:Component(TransformType),dirtyFlag(false), dirtyFlagInverse(false)
+:Component(TransformType),dirtyFlag(false), dirtyFlagInverse(false), scale(1,1,1)
 {}
 
 Transform::~Transform(void)
@@ -21,7 +21,7 @@ Transform::~Transform(void)
 void Transform::UpdateIfDirty(){
     if (dirtyFlag){
         rotation.GetMatrix(&localTransform);
-        localTransform = Matrix44::Scale(scale)*Matrix44::Translate(position)*localTransform; // todo is this the right order???
+        localTransform = Matrix44::Scale(scale)* Matrix44::Translate(position) *localTransform;
         dirtyFlag = false;
     }
 }
@@ -29,7 +29,7 @@ void Transform::UpdateIfDirty(){
 void Transform::UpdateInverseIfDirty(){
     if (dirtyFlagInverse){
         rotation.GetMatrix(&localTransformInverse);
-        localTransformInverse = localTransformInverse.Transpose()*Matrix44::Translate(-position)*Matrix44::ScaleInverse(scale); // todo is this in right order?
+        localTransformInverse = localTransformInverse.Transpose()* Matrix44::Translate(-position) * Matrix44::ScaleInverse(scale);
         dirtyFlagInverse = false;
     }
 }
@@ -39,13 +39,18 @@ float *Transform::GetLocalTransform(){
     return localTransform.GetReference();
 }
 
+float *Transform::GetLocalTransformInverse(){
+    UpdateInverseIfDirty();
+    return localTransformInverse.GetReference();
+}
+
 
 Vector3 Transform::GetPosition() const {
-    
+    return position;
 }
 
 Quaternion Transform::GetRotation() const {
-    
+    return rotation;
 }
 
 Vector3  Transform::GetScale() const{
@@ -55,17 +60,24 @@ Vector3  Transform::GetScale() const{
 void Transform::SetPosition(Vector3 &newPosition) {
     position = newPosition;
     dirtyFlag = true;
+    dirtyFlagInverse = true;
 }
 
 void Transform::SetRotation(Quaternion &quaternion) {
-
+    this->rotation = quaternion;
+    dirtyFlag = true;
+    dirtyFlagInverse = true;
 }
 
 void Transform::SetRotation(Vector3 &euler){
     this->rotation = Quaternion();
+    dirtyFlag = true;
+    dirtyFlagInverse = true;
 }
 
 void Transform::SetScale(Vector3 &scale){
     this->scale = scale;
+    dirtyFlag = true;
+    dirtyFlagInverse = true;
 }
 }
